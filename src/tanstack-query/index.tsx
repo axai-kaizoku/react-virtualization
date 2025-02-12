d2b6fc9599ac1ab84b416cc77d412011dc78dc1b
Akshay <02b3akshay@gmail.com>
2025-02-12 04:47:38 +0000

    add react-query setup

--- src/tanstack-query/index.tsx
+++ src/tanstack-query/index.tsx
@@ -2,20 +2,31 @@ import { useQuery } from "@tanstack/react-query"
 import { getTodos } from "../queries/query"
 
 const TanstackQuery = () => {
-  const { data, isPending } = useQuery({
+  const { data, isPending, refetch, error } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
   })
+
+  if (error) {
+    alert("something went wrong!")
+  }
+
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
-        <div className="w-[50dvw]">
+      <div className="w-full h-screen flex flex-col gap-4 justify-center items-center">
+        <div className="w-[70dvw]">
           {isPending ? (
-            <>Loading..</>
+            <img src="/spinner.svg" alt="spinner" className="size-14" />
           ) : (
             <>{JSON.stringify(data.slice(0, 10))}</>
           )}
         </div>
+        <button
+          onClick={() => refetch()}
+          className="bg-amber-50 px-2 py-1 rounded-md text-black font-mono cursor-pointer"
+        >
+          refetch
+        </button>
       </div>
     </>
   )
